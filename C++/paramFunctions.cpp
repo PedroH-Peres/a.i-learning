9e6dfea0b3fbe9c672f6d0c886869bf4cfe2cd9d
Pedro Henrique Bohling Peres <pedrohperescode@gmail.com>
2024-09-09 14:56:13 -0300

    Functions with Parameters

--- C++/paramFunctions.cpp
+++ C++/paramFunctions.cpp
@@ -14,9 +14,17 @@ int sumTwoNumbers(int a, int b){
     return a+b;
 }
 
+void repeatHello(int quantity){
+    for(int i = 0; i < quantity;i++){
+        cout << " [" << i+1 << "]" <<"Hello World! \n";
+    }
+}
+
 int main(){
     int num1 = 2;
     int num2 = 3;
+
+    int q = 4;
 
     cout << "Hello World!\n";
 
@@ -24,4 +32,7 @@ int main(){
     cout << num2 << "-> Par(0) ou Impar(1): " << evenOrOdd(num2) << "\n";
 
     cout << "Soma de " << num1 << " + " << num2 << ": " << sumTwoNumbers(num1, num2) << "\n";
+
+    repeatHello(q);
+    
 }
